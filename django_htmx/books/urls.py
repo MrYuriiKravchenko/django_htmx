1e1d2591d14183e2f78f7648973b02176f298a82
MrYuriiKravchenko <MisterKrava@yandex.ru>
2024-11-01 19:54:00 +0300

    button and view

--- django_htmx/books/urls.py
+++ django_htmx/books/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import book_list, create_book
+from .views import (
+    book_list,
+    create_book,
+    update_book_details,
+    book_detail,
+    delete_book,
+    update_book_status,
+)
+
 
 urlpatterns = [
     path("", book_list, name="book_list"),
     path("create_book/", create_book, name="create_book"),
+    path("update_book_details/<int:pk>/", update_book_details, name="update_book_details"),
+    path("book_detail/<int:pk>/", book_detail, name="book_detail"),
+    path("delete_book/<int:pk>/", delete_book, name="delete_book"),
+    path("update_book_status/<int:pk>/", update_book_status, name="update_book_status"),
 ]
--- django_htmx/books/views.py
+++ django_htmx/books/views.py
@@ -1,9 +1,26 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_http_methods
+from .forms import BookCreateForm, BookEditForm
 
 from .models import Book
 from .forms import BookCreateForm
+
+def update_book_details(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BookEditForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save()
+            return render(request, 'partial_book_detail.html', {'book': book})
+    else:
+        form = BookEditForm(instance=book)
+    return render(request, 'partial_book_update_form.html', {'book': book, 'form': form})
+
+@require_http_methods(['GET'])
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'partial_book_detail.html', {'book': book})
 
 @require_http_methods(['GET'])
 def book_list(request):
@@ -16,4 +33,17 @@ def create_book(request):
     form = BookCreateForm(request.POST)
     if form.is_valid():
         book = form.save()
+    return render(request, 'partial_book_detail.html', {'book': book})
+
+@require_http_methods(['DELETE'])
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return HttpResponse()
+
+@require_http_methods(['PATCH'])
+def update_book_status(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.read = not book.read
+    book.save()
     return render(request, 'partial_book_detail.html', {'book': book})
